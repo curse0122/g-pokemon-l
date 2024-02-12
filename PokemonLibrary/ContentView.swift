1dacbdabed62485977a8d8d01785d046361ee226
nier <curse0122@gmail.com>
2024-02-12 16:08:27 +0800

    Initial commit

---
+++ PokemonLibrary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokemonLibrary
+//
+//  Created by nier on 2024/2/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokemonLibrary/MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainApp.swift
+//  PokemonLibrary
+//
+//  Created by nier on 2024/2/12.
+//
+
+import SwiftUI
+
+@main
+struct MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
